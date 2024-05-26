72158da81b9f49b4ab5ca680be504561f423b139
onlygarrett <rumschik.2@gmail.com>
2024-05-26 09:54:28 -0400

    a lil cli parsing

--- dealscraper/config/builder.py
+++ dealscraper/config/builder.py
@@ -1,8 +1,43 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
+import argparse
 import yaml
 
 from dealscraper.config.handler import DealScraperConfigHandler
+
+
+# TODO: IMPLEMENT
+def parse_cli(args: List[str]) -> Dict[str, Any]:
+    """
+    Parse the command line arguments into a dictionary.
+
+    This function uses argparse module to parse command line arguments. It creates a base parser and a subparser for 
+    'load' and 'inline' options. The 'inline' option is used to pass arguments by CLI.
+
+    Parameters:
+    args (List[str]): A list of command line arguments.
+
+    Returns:
+    Dict[str, Any]: A dictionary containing the parsed command line arguments.
+
+    Raises:
+    argparse.ArgumentError: If the command line arguments are not provided or are invalid.
+
+    """
+    base_parser = argparse.ArgumentParser()
+
+    base_subparsers = base_parser.add_subparsers(
+        dest='load | inline',
+        help='Pass load with a YAML config or inline to pass args by CLI.',
+        required=True,
+    )
+
+    # We are using CLI for all arguments.
+    cli_parser = base_subparsers.add_parser(
+        'inline',
+        help='Configure search query and data providers via CLI.',
+    )
+    return vars(base_parser.parse_args(args))
 
 
 def build_config() -> Dict[str, Any]:
